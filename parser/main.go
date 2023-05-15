4c09b7a4523b7275888db67334943dcdab116004
ddlifter <132294753+ddlifter@users.noreply.github.com>
2023-05-15 20:28:26 +0300

    all pages

--- parser/main.go
+++ parser/main.go
@@ -16,14 +16,22 @@ func main() {
 		colly.AllowedDomains("hh.ru"),
 	)
 
+	var items []Item
+
 	c.OnHTML("div[class=vacancy-serp-item-body__main-info]", func(h *colly.HTMLElement) {
 		item := Item{
 			Name:  h.ChildText("h3.bloko-header-section-3"),
 			Price: h.ChildText("span[class=bloko-header-section-3]"),
 		}
-		fmt.Println(item)
+		items = append(items, item)
+	})
+
+	c.OnHTML("[class=bloko-button]", func(h *colly.HTMLElement) {
+		next_page := h.Request.AbsoluteURL(h.Attr("href"))
+		c.Visit(next_page)
 	})
 
 	c.Visit("https://hh.ru/search/vacancy?text=Golang&area=1")
+	fmt.Println(items)
 
 }
